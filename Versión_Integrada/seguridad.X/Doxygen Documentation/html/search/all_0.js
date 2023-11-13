d81f185de111a7f3038ec9518e942c2c20caa9d1
Sebastianfiet <146495352+Sebastianfiet@users.noreply.github.com>
2023-11-13 17:33:22 -0500

    Delete Versión_Integrada/seguridad.X/Doxygen Documentation directory

--- Versión_Integrada/seguridad.X/Doxygen Documentation/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['fuses_2eh_0',['fuses.h',['../fuses_8h.html',1,'']]]
-];
--- Versión_Integrada/seguridad.X/Doxygen Documentation/html/search/files_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mainseguridad_2ec_0',['mainseguridad.c',['../mainseguridad_8c.html',1,'']]]
-];
